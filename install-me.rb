14cf96895b767a2c89b99e3a5eed6c8809f55655
Greg Little <glittle@gmail.com>
2015-09-28 18:27:08 -0700

    blah

--- install-me.rb
+++ install-me.rb
@@ -1,3 +1,17 @@
 
-require 'open-uri'
-eval(open('http://dglittle.github.io/ezhome/plugin.rb').read)
+# require 'open-uri'
+# eval(open('http://dglittle.github.io/ezhome/plugin.rb').read)
+
+p 'version 19'
+
+require 'sketchup.rb'
+require 'net/http'
+require 'net/https'
+require 'base64'
+require 'json'
+
+UI.add_context_menu_handler do |context_menu|
+	context_menu.add_item("ezhome test plugin") {
+		UI.messagebox('Hello World!')
+	}
+end
