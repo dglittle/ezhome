c6c0a93a9b1e63f7ea84245dc398718a94658b28
Greg Little <glittle@gmail.com>
2015-10-16 12:46:22 -0700

    blah

--- plugin-debug.rb
+++ plugin-debug.rb
@@ -11,9 +11,7 @@ UI.add_context_menu_handler do |context_menu|
 	context_menu.add_item("ezez-debug") {
 		d = UI::WebDialog.new("ezez-debug", false, "ezez-debug", 600, 600, 0, 0, true)
 		d.add_action_callback("eval") do |web_dialog, action_name|
-			p 'action_name:'
-			p action_name
-			eval(action_name)
+			eval(web_dialog.get_element_value(action_name))
 		end
 		d.set_url('http://dglittle.github.io/ezhome/index-debug.html')
 		d.show()
